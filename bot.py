cf15a24b27de9c757ba33f00b48f59c5fbb62795
filestorebotbyaccbot <filestorebotbyaccbot@gmail.com>
2026-06-07 18:17:33 +0530

    Refactor bot.py with start and stop methods
    Refactor bot.py to improve formatting and add start/stop methods.

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
-    TechVJUser = Client("TechVJ", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
-    TechVJUser.start()
+	TechVJUser = Client("TechVJ", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
+	TechVJUser.start()
 else:
     TechVJUser = None
 
@@ -24,11 +24,14 @@ class Bot(Client):
             sleep_threshold=5
         )
 
+      
     async def start(self):
+            
         await super().start()
         print('Bot Started Powered By @VJ_Bots')
 
     async def stop(self, *args):
+
         await super().stop()
         print('Bot Stopped Bye')
 
