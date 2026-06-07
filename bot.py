4a2b5f15278574baff70cd1b4178c97b68dc4c99
filestorebotbyaccbot <filestorebotbyaccbot@gmail.com>
2026-06-07 17:56:21 +0530

    Refactor bot.py by removing unused imports

--- bot.py
+++ bot.py
@@ -2,12 +2,8 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-import os
-import threading
-from http.server import SimpleHTTPRequestHandler, HTTPServer
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
-
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
     TechVJUser = Client("TechVJ", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
