2698f8261a1554d7d223db6ed7cd8fa069227c38
filestorebotbyaccbot <filestorebotbyaccbot@gmail.com>
2026-06-07 17:26:28 +0530

    Fix indentation and remove unnecessary lines in bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
 
--------------------------------------
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
     TechVJUser = Client("TechVJ", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
